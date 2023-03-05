864f2e6575beb05e9554f0ab515635f555f37d0a
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2023-03-05 01:18:23 -0300

    Configuracion de carusel de logos

--- src/components/home/LogosList.tsx
+++ src/components/home/LogosList.tsx
@@ -1,15 +1,35 @@
-import { getImageURL } from '../../helpers/file_helpers'
-import { Logo } from './Logo'
-import './logos.css'
+import Carousel from 'nuka-carousel/lib/carousel';
+import { easeCircleOut, easeElasticOut } from 'd3-ease';
+
+import { Logo } from './Logo';
+import './logos.css';
 
 interface Props {
-    logos: string[]
+    logos: string[];
 }
-export const LogosList = ({logos}: Props) => {
-  return (
-    <div className='logos__container'>
-        {logos.map(logo => <Logo logo_name={logo} key={logo} />)}
-    </div>
-
-  )
-}
+export const LogosList = ({ logos }: Props) => {
+    return (
+        <div className='logos__container'>
+            <Carousel style={{zIndex:-1}}
+				defaultControlsConfig={{
+					nextButtonStyle:{display:'none'},
+					prevButtonStyle:{display:'none'},
+					pagingDotsStyle:{zIndex: -1},
+				}}
+                autoplay
+				cellSpacing={25}
+				cellAlign='center'
+                wrapAround={true} slidesToShow={1}
+				autoplayInterval={1000}
+				animation='zoom'
+				speed={1000}
+				pauseOnHover
+				easing={easeCircleOut} edgeEasing={easeElasticOut}
+            >
+                {logos.map((logo) => (
+                    <Logo logo_name={logo} key={logo} />
+                ))}
+            </Carousel>
+        </div>
+    );
+};
